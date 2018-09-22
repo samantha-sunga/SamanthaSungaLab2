c013c0596e16bfb8f48ffa6274cdbdc0d38e0ad9
samantha-sunga <2015081443@ust-ics.mygbiz.com>
2018-09-22 09:38:54 +0800

    push

---
+++ app/src/main/java/com/example/patricia/myapplication/Lab1_SamanthaSunga.java
@@ -0,0 +1,13 @@
+package com.example.patricia.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lab1_SamanthaSunga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab1__samantha_sunga);
+    }
+}
